a5155d4e4cadb26a317b11684c386d81d8db63c4
spookeyy <pangasmeshack@gmail.com>
2025-06-09 18:59:50 +0300

    use nativewind as preset array instead of plugins

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,31 @@
-module.exports = (api) => {
-  api.cache(true)
+//claude
+module.exports = function (api) {
+  api.cache(true);
   return {
-    presets: ["babel-preset-expo"],
-    plugins: ["nativewind/babel"],
-  }
-}
+    presets: [
+      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+      "nativewind/babel",
+    ],
+  };
+};
+
+// deepseek
+// module.exports = function (api) {
+//   api.cache(true);
+//   return {
+//     presets: [
+//       [
+//         "babel-preset-expo",
+//         {
+//           jsxRuntime: "automatic",
+//           jsxImportSource: "nativewind",
+//         },
+//       ],
+//     ],
+//     plugins: [
+//       "nativewind/babel",
+//       // Add react-native-reanimated plugin if using animations
+//       "react-native-reanimated/plugin",
+//     ],
+//   };
+// };
