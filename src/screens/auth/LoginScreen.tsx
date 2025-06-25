dbce1afae6c69739e9303c381f03db3a360c3e9f
spookeyy <pangasmeshack@gmail.com>
2025-06-25 03:36:59 +0300

    updated login screen

--- src/screens/auth/LoginScreen.tsx
+++ src/screens/auth/LoginScreen.tsx
@@ -1,5 +1,3 @@
-"use client"
-
 import { useState } from "react"
 import { View, Text, TextInput, TouchableOpacity, SafeAreaView, ActivityIndicator } from "react-native"
 import { Ionicons } from "@expo/vector-icons"
